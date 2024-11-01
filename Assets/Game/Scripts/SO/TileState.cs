82f0aa65d36955ac58383a4fb0a9d6c570bcc8e9
bugra <bugra.ksr@gmail.com>
2024-11-01 16:09:00 +0300

    So & Canvas Update

---
+++ Assets/Game/Scripts/SO/TileState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TileState")]
+public class TileState : ScriptableObject
+{
+    public Color backgroundColor;
+    public Color textColor;
+}
